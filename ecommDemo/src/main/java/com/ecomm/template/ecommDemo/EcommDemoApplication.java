224840762a26ed13f0ff4dce524881f8b39dbbff
Bishoy Mokhless <65623158+BishoyMokhless@users.noreply.github.com>
2022-11-12 12:17:24 +0200

    Add files via upload

---
+++ ecommDemo/src/main/java/com/ecomm/template/ecommDemo/EcommDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.template.ecommDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommDemoApplication.class, args);
+	}
+
+}
---
+++ ecommDemo/src/test/java/com/ecomm/template/ecommDemo/EcommDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.template.ecommDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
